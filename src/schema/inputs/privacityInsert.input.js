0f70b50ead09fca2e2316da330a4bf27d344f565
adria <adria_4_@hotmail.com>
2018-03-25 19:19:31 +0200

    Pasando los Inputs a Strings

--- src/schema/inputs/privacityInsert.input.js
+++ src/schema/inputs/privacityInsert.input.js
@@ -1,30 +1,9 @@
 'use strict';
 
-const {
-    GraphQLNonNull,
-    GraphQLInt,
-    GraphQLString,
-    GraphQLInputObjectType
-} = require('graphql');
-
-const resolver = require('../resolvers/privacity.resolvers');
-const Image = require('../types/image.type');
-
-module.exports = new GraphQLInputObjectType({
-    name: 'PrivacityInputInsert',
-    description: '...',
-    fields: () => ({
-        user_id: {
-            type: new GraphQLNonNull(GraphQLInt)
-        },
-        search: {
-            type: new GraphQLNonNull(GraphQLString)
-        },
-        posts: {
-            type: new GraphQLNonNull(GraphQLString)
-        },
-        albums: {
-            type: new GraphQLNonNull(GraphQLString)
-        },
-    })
-});
+module.exports = `
+input PrivacityInputInsert {
+    user_id: Int!
+    search: String!
+    posts: String!
+    albums: String!
+}`;
--- src/schema/inputs/privacityUpdate.input.js
+++ src/schema/inputs/privacityUpdate.input.js
@@ -1,33 +1,10 @@
 'use strict';
 
-const {
-    GraphQLNonNull,
-    GraphQLInt,
-    GraphQLString,
-    GraphQLInputObjectType
-} = require('graphql');
-
-const resolver = require('../resolvers/visibility.resolvers');
-const Image = require('../types/image.type');
-
-module.exports = new GraphQLInputObjectType({
-    name: 'VisibilityInputUpdate',
-    description: '...',
-    fields: () => ({
-        id: {
-            type: new GraphQLNonNull(GraphQLInt)
-        },
-        user_id: {
-            type: new GraphQLNonNull(GraphQLInt)
-        },
-        search: {
-            type: GraphQLString
-        },
-        posts: {
-            type: GraphQLString
-        },
-        albums: {
-            type: GraphQLString
-        },
-    })
-});
+module.exports = `
+input VisibilityInputUpdate {
+    id: Int!
+    user_id: Int!
+    search: String
+    posts: String
+    albums: String!
+}`;
