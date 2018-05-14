873e5d9d997bea9b20515dd9bc6ad90d38983f5e
adria <adria_4_@hotmail.com>
2018-05-14 23:55:23 +0200

    Añadida constante

--- src/schema/resolvers/mutations/upload.mutations.resolvers.js
+++ src/schema/resolvers/mutations/upload.mutations.resolvers.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const { saveFile } = require('./../../../helpers')
-// const sharp = require('sharp');
+    // const sharp = require('sharp');
+
 
 module.exports = {
-  uploadImage: async (parent, args, context, info) => {
-    const file = await saveFile(args.file, './static/images');
-    return context.db.Image.create({
-      small: file.computedName,
-      medium: file.computedName
-    });
-  }
+    uploadImage: async(parent, args, context, info) => {
+        const file = await saveFile(args.file, process.env.IMAGES_FOLDER);
+        return context.db.Image.create({
+            small: file.computedName,
+            medium: file.computedName
+        });
+    }
 };
 
 /*
