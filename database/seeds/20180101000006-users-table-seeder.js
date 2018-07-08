e911b05e193e3948903a44e91ee7964ee2652c94
Denís <denisnavarroalcaide94@gmail.com>
2018-07-08 23:44:30 +0200

    chore: imageview user on seeds

--- database/seeds/20180101000006-users-table-seeder.js
+++ database/seeds/20180101000006-users-table-seeder.js
@@ -1,7 +1,16 @@
 const factory = require('database/factories');
 
 function seed(knex) {
-  return knex.insert(factory('users', 20)).into('users');
+  return knex.insert([
+    factory('users', 1, () => ({
+      username: 'imageview',
+      email: 'info@imageview.com',
+      name: 'Image',
+      lastname: 'View',
+      role: 'admin'
+    })),
+    ...factory('users', 19)
+  ]).into('users');
 }
 
 module.exports = {seed};
