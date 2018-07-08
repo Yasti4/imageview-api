582ac958ba210a92d53aa6b21df1f672f73659db
Denís <denisnavarroalcaide94@gmail.com>
2018-07-08 23:00:03 +0200

    chore: ups filenames

--- database/seeds/20180101000002-tags-table-seeder.js
+++ database/seeds/20180101000002-tags-table-seeder.js
@@ -1,9 +1,6 @@
 const factory = require('database/factories');
 
-console.log('Tags: AFUERA');
-
 function seed(knex) {
-  console.log('Tags: DENTRO');
   return knex.insert(factory('tags', 40)).into('tags');
 }
 
